2b2e4595954e4741c6e863bb4c0e9863356a9f0a
Puppy Pc <freyanoir@gmail.com>
2020-02-24 02:34:09 +0200

    first commit

---
+++ src/views/Programme.jsx
@@ -0,0 +1,15 @@
+import ProgrammeTable from "../components/ProgrammeTable"
+
+import React, { Component } from 'react';
+
+class Programme extends Component {
+    render() {
+        return (
+            <div>
+                <ProgrammeTable/>
+            </div>
+        );
+    }
+}
+
+export default Programme;
